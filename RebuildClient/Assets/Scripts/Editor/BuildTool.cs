6f7006403d4c37c052cc9575dc5c3bf049cdd1f6
Doddler <doddler@gmail.com>
2022-03-18 05:13:53 -0600

    Added a real build process so I don't have to go through all hell to upload a new build.

---
+++ RebuildClient/Assets/Scripts/Editor/BuildTool.cs
@@ -0,0 +1,190 @@
+using System.IO;
+using System.IO.Compression;
+using Assets.Scripts.MapEditor.Editor;
+using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Build;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class BuildTool : MonoBehaviour
+{
+
+    [MenuItem("Build/Build Everything", false, 1)]
+    public static void TheFullMonte()
+    {
+        RagnarokMapImporterWindow.UpdateAddressables();
+
+        var group = EditorUserBuildSettings.selectedBuildTargetGroup;
+        var target = EditorUserBuildSettings.activeBuildTarget;
+
+        //webgl
+        BuildForPlatform(BuildTargetGroup.WebGL, BuildTarget.WebGL, "Build/WebGL/ragnarok", true, true);
+
+        //windows64
+        BuildForPlatform(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64, "Build/PC/RebuildClient.exe", true, true);
+        
+        //restore
+        EditorUserBuildSettings.SwitchActiveBuildTarget(group, target);
+        
+        //zip PC build
+        ZipPcBuildIntoWebGL();
+    }
+    
+    [MenuItem("Build/Build Everything (No Addressables)", false, 2)]
+    public static void EverythingWithoutAddressables()
+    {
+        //webgl
+        BuildForPlatform(BuildTargetGroup.WebGL, BuildTarget.WebGL, "Build/WebGL", false, false);
+
+        //windows64
+        BuildForPlatform(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64, "Build/PC/RebuildClient.exe", false, false);
+        
+        //zip PC build
+        ZipPcBuildIntoWebGL();
+    }
+
+    //[MenuItem("Build/Test the zip thing", false, 2000)]
+    private static void ZipPcBuildIntoWebGL()
+    {
+
+        if (Directory.Exists("Build/RagnarokRebuild"))
+            Directory.Delete("Build/RagnarokRebuild", true);
+
+        //the double folder name is dumb but we want the zip file to have a folder in it so it's gotta be done
+        Directory.CreateDirectory("Build/RagnarokRebuild/RagnarokRebuild");
+        foreach (var path in Directory.GetFiles("Build/PC"))
+            File.Copy(path, Path.Combine("Build/RagnarokRebuild/RagnarokRebuild", Path.GetFileName(path)));
+
+        CopyFilesRecursively("Build/PC/RebuildClient_Data", "Build/RagnarokRebuild/RagnarokRebuild/RebuildClient_Data");
+
+        if (File.Exists("Build/WebGL/ragnarok/RagnarokRebuild.zip"))
+            File.Delete("Build/WebGL/ragnarok/RagnarokRebuild.zip");
+        ZipFile.CreateFromDirectory("Build/RagnarokRebuild", "Build/WebGL/ragnarok/RagnarokRebuild.zip");
+    }
+
+    private static void BuildForPlatform(BuildTargetGroup group, BuildTarget platform, string location, bool updateAddressables, bool swapToPlatform)
+    {
+        Debug.Log($"Building for platform {group}:{platform} in destination folder {location} ({(updateAddressables ? "with" : "without")} addressables)");
+        if(swapToPlatform)
+            EditorUserBuildSettings.SwitchActiveBuildTarget(group, platform);
+
+        if (updateAddressables)
+            BuildTool.UpdateAddressables();
+
+        var folder = location;
+        if (folder.ToLower().EndsWith(".exe"))
+            folder = Path.GetDirectoryName(folder);
+
+        if (!Directory.Exists(folder))
+            Directory.CreateDirectory(folder);
+
+        if (platform == BuildTarget.WebGL)
+        {
+            //remove old build folders
+            foreach(var dir in Directory.GetDirectories(location, "*", SearchOption.AllDirectories))
+                if(dir.Contains("Build_"))
+                    Directory.Delete(dir, true);
+        }
+
+        var options = new BuildPlayerOptions();
+        options.locationPathName = location;
+        options.targetGroup = group;
+        options.target = platform;
+        options.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer(options);
+    }
+
+
+    private static void CopyFilesRecursively(string sourcePath, string targetPath)
+    {
+        //Now Create all of the directories
+        foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+        {
+            Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+        }
+
+        //Copy all the files & Replaces any files with the same name
+        foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+        {
+            var relative = Path.GetRelativePath(sourcePath, newPath);
+            File.Copy(newPath, Path.Combine(targetPath, relative), true);
+        }
+    }
+
+    [MenuItem("Build/Build WebGL", false, 50)]
+    public static void BuildWebGL()
+    {
+        BuildForPlatform(BuildTargetGroup.WebGL, BuildTarget.WebGL, "Build/WebGL", false, false);
+    }
+
+    [MenuItem("Build/Build Windows", false, 51)]
+    public static void BuildWindows()
+    {
+        BuildForPlatform(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64, "Build/PC/RebuildClient.exe", false, false);
+    }
+
+    private static void UpdateAddressables()
+    {
+        var path = ContentUpdateScript.GetContentStateDataPath(false);
+        var settings = AddressableAssetSettingsDefaultObject.Settings;
+        ContentUpdateScript.BuildContentUpdate(settings, path);
+
+    }
+
+    [MenuItem("Build/Update Addressables (Current Platform)", false, 100)]
+    public static void UpdateAddressablesBuild()
+    {
+        RagnarokMapImporterWindow.UpdateAddressables();
+        UpdateAddressables();
+
+    }
+
+
+    [MenuItem("Build/Update Addressables (All Platforms)", false, 100)]
+    public static void UpdateAddressablesBuildAll()
+    {
+        RagnarokMapImporterWindow.UpdateAddressables();
+
+        var group = EditorUserBuildSettings.selectedBuildTargetGroup;
+        var target = EditorUserBuildSettings.activeBuildTarget;
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.WebGL, BuildTarget.WebGL);
+
+        UpdateAddressables();
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64);
+
+        UpdateAddressables();
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(group, target);
+    }
+
+
+    [MenuItem("Build/Full Addressables Rebuild/Build All", false, 150)]
+    public static void FullAddressablesBuild()
+    {
+        var group = EditorUserBuildSettings.selectedBuildTargetGroup;
+        var target = EditorUserBuildSettings.activeBuildTarget;
+
+        AddressableAssetSettings.CleanPlayerContent();
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.WebGL, BuildTarget.WebGL);
+
+        RagnarokMapImporterWindow.UpdateAddressables();
+        AddressableAssetSettings.BuildPlayerContent();
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64);
+
+        RagnarokMapImporterWindow.UpdateAddressables();
+        AddressableAssetSettings.BuildPlayerContent();
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(group, target);
+
+        //var path = ContentUpdateScript.GetContentStateDataPath(false);
+
+        //Debug.Log(path);
+    }
+
+}
---
+++ RebuildClient/Assets/Scripts/Editor/WebGLUnfucker.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using DateTime = System.DateTime;
+
+public static class WebGLUnfucker
+{
+    [PostProcessBuild]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        if (target != BuildTarget.WebGL)
+            return;
+
+        var buildFolder = "Build_" + DateTime.Now.ToString("yyyy-mm-dd-hh-mm");
+        Directory.Move(Path.Combine(pathToBuiltProject, "Build"), Path.Combine(pathToBuiltProject, buildFolder));
+
+        var indexPath = Path.Combine(pathToBuiltProject, "index.html");
+        var indexFile = File.ReadAllText(indexPath);
+        indexFile = indexFile.Replace("buildUrl = \"Build\";", $"buildUrl = \"{buildFolder}\";");
+        File.WriteAllText(indexPath, indexFile);
+
+
+    }
+}
