c4b1922f4af2947186918965adc5f40c4ebae00d
Doddler <doddler@gmail.com>
2023-09-08 19:00:40 -0600

    Revert max view distance to a sane amount.

--- RoRebuildServer/RoRebuildServer/Data/ServerConfig.cs
+++ RoRebuildServer/RoRebuildServer/Data/ServerConfig.cs
@@ -4,7 +4,7 @@ namespace RoRebuildServer.Data;
 
 public static class ServerConfig
 {
-    public const int MaxViewDistance = 310;
+    public const int MaxViewDistance = 21;
     public const int MaxAoESize = 9;
     public const int InitialConnectionCapacity = 40;
 
