af07a4f84e6ae3abad086b97ea32541adc1daa38
Doddler <doddler@gmail.com>
2024-08-19 02:41:51 -0600

    Added missing ActionTextDisplay source file.

---
+++ RebuildClient/Assets/Scripts/UI/ActionTextDisplay.cs
@@ -0,0 +1,54 @@
+﻿using Assets.Scripts.SkillHandlers;
+using Assets.Scripts.Sprites;
+using RebuildSharedData.Enum;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class ActionTextDisplay : MonoBehaviour
+    {
+        public GameObject Container;
+        public TextMeshProUGUI TopTextArea;
+        public TextMeshProUGUI BottomTextArea;
+        public RectTransform TopLayoutGroup;
+        public RectTransform BottomLayoutGroup;
+
+        private CharacterSkill activeSkill;
+        private int selectedSkillLevel;
+
+        public void SetSkillTargeting(CharacterSkill skill, int level)
+        {
+            Container.SetActive(true);
+
+            if (activeSkill == skill && selectedSkillLevel == level)
+                return;
+
+            var skillData = ClientDataLoader.Instance.GetSkillData(skill);
+            if (skillData.AdjustableLevel)
+                TopTextArea.text = $"{skillData.Name} Lv {level}";
+            else
+                TopTextArea.text = $"{skillData.Name}";
+
+            BottomTextArea.text = "Select a target.";
+
+            activeSkill = skill;
+            selectedSkillLevel = level;
+            
+            
+            LayoutRebuilder.ForceRebuildLayoutImmediate(TopLayoutGroup);
+            LayoutRebuilder.ForceRebuildLayoutImmediate(BottomLayoutGroup);
+            
+            TopTextArea.ForceMeshUpdate();
+            BottomTextArea.ForceMeshUpdate();
+            
+        }
+
+        public void EndActionTextDisplay()
+        {
+            Container.SetActive(false);
+        }
+    }
+}
