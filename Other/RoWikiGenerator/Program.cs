c9f23378d5f8b3bf61cc8ec1e598b16f8ddcca7e
Doddler <doddler@gmail.com>
2025-06-03 00:34:45 -0600

    Refactored wiki generator to use razor components.

--- Other/RoWikiGenerator/Program.cs
+++ Other/RoWikiGenerator/Program.cs
@@ -1,53 +1,120 @@
 ﻿using System.Diagnostics;
 using System.Text;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using RebuildSharedData.Enum;
 using RebuildSharedData.Enum.EntityStats;
 using RoRebuildServer.Data;
 using RoRebuildServer.Data.Map;
+using RoRebuildServer.Data.Monster;
 using RoRebuildServer.Database;
 using RoRebuildServer.Logging;
 using RoRebuildServer.Networking;
 using RoRebuildServer.Simulation;
 using RoRebuildServer.Simulation.Pathfinding;
+using RoWikiGenerator.Pages;
 
 namespace RoWikiGenerator;
 
-internal class Program
+public class Program
 {
+    //static void FixName()
+    //{
+    //    var lines = File.ReadAllLines(@"G:\Projects2\Ragnarok\Zone\npcdata\mobname.def");
+    //    foreach (var line in lines)
+    //    {
+    //        var s = line.Split(' ');
+    //        if (s.Length < 2)
+    //            continue;
 
+    //        var id = int.Parse(s[1]);
+    //        var txt = s[0].ToLower();
+    //        var path = $"../../../images/monsters/{id}.png";
+    //        if (!File.Exists(path))
+    //            continue;
 
-    static void FixName()
+    //        File.Copy(path, $"../../../images/rebuildmonsters/{txt}.png");
+
+    //        Console.WriteLine(path);
+
+    //    }
+    //}
+
+    public static async Task<string> RenderMonsters(MonsterModel model)
     {
-        var lines = File.ReadAllLines(@"G:\Projects2\Ragnarok\Zone\npcdata\mobname.def");
-        foreach (var line in lines)
+        var htmlRenderer = GetRenderer();
+
+        var html = await htmlRenderer.Dispatcher.InvokeAsync(async () =>
         {
-            var s = line.Split(' ');
-            if (s.Length < 2)
-                continue;
+            var dictionary = new Dictionary<string, object?>
+            {
+                { "Model", model }
+            };
 
-            var id = int.Parse(s[1]);
-            var txt = s[0].ToLower();
-            var path = $"../../../images/monsters/{id}.png";
-            if (!File.Exists(path))
-                continue;
+            var parameters = ParameterView.FromDictionary(dictionary);
+            var output = await htmlRenderer.RenderComponentAsync<RebuildMonsters>(parameters);
+            return output.ToHtmlString();
+        });
 
-            File.Copy(path, $"../../../images/rebuildmonsters/{txt}.png");
-
-            Console.WriteLine(path);
-
-        }
+        return html;
     }
 
-    static void Main(string[] args)
+    public static async Task<string> InsertContentIntoTemplate(string content, string title)
+    {
+        var htmlRenderer = GetRenderer();
+
+        var html = await htmlRenderer.Dispatcher.InvokeAsync(async () =>
+        {
+            var dictionary = new Dictionary<string, object?>
+            {
+                { "Title", title },
+                { "Content", content },
+            };
+
+            var parameters = ParameterView.FromDictionary(dictionary);
+            var output = await htmlRenderer.RenderComponentAsync<PageTemplate>(parameters);
+            return output.ToHtmlString();
+        });
+
+        return html;
+    }
+
+    private static IServiceCollection services;
+    private static IServiceProvider serviceProvider;
+    private static ILoggerFactory loggerFactory;
+
+    public static HtmlRenderer GetRenderer()
+    {
+        return new HtmlRenderer(serviceProvider, loggerFactory);
+    }
+
+    public static void SetUpServiceProvider()
+    {
+        services = new ServiceCollection();
+        services.AddLogging();
+
+        serviceProvider = services.BuildServiceProvider();
+        loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+    }
+
+    public class MonsterModel
+    {
+        public Dictionary<int, List<(string map, MapSpawnRule spawn)>> monsterMapSpawns;
+        public Dictionary<string, string> sharedIcons;
+        public List<MonsterDatabaseInfo> monsters;
+    }
+
+    static async Task Main(string[] args)
     {
         ServerLogger.Log("Ragnarok Rebuild Zone Server, starting up!");
 
         //FixName();
         //DistanceCache.Init();
         //RoDatabase.Initialize();
-
 
         var sharedIcons = new Dictionary<string, string>();
 
@@ -64,6 +131,9 @@ internal class Program
         Console.WriteLine($"Max spawn time: {ServerConfig.DebugConfig.MaxSpawnTime}");
 
         DataManager.Initialize(true);
+
+        SetUpServiceProvider();
+
 
         var monsterMapSpawns = new Dictionary<int, List<(string map, MapSpawnRule spawn)>>();
 
@@ -99,165 +169,17 @@ internal class Program
             //.OrderBy(m => m.Level).ThenBy(m => m.Name).ToList();
             .OrderBy(m => m.Id).ToList();
 
-        var txtOut = new StringBuilder();
-
-        foreach (var m in monsters)
+        var model = new MonsterModel()
         {
-            if (m.Id < 4000 || m.Id == 5000)
-                continue;
-            var mvp = "";
-            if (DataManager.MvpMonsterCodes.Contains(m.Code))
-                mvp = "MVP Boss";
-            if (m.Special == CharacterSpecialType.Boss && mvp == "")
-                mvp = "Boss";
-            var name = m.Id switch
-            {
-                4146 => "Whisper (Stationary)",
-                4147 => "Whisper (Giant)",
-                6000 => "Moonlight Flower (Clone)",
-                6001 => "Thief Bug Egg (Summon)",
-                _ => m.Name
-            };
-            var special = "";
+            monsterMapSpawns = monsterMapSpawns,
+            sharedIcons = sharedIcons,
+            monsters = monsters
+        };
 
-            var sprite = m.Code.ToLower();
-            var magicMin = m.Int + m.Int / 7 * m.Int / 7;
-            var magicMax = m.Int + m.Int / 5 * m.Int / 5;
-            if (magicMin < m.AtkMin / 6)
-                magicMin = m.AtkMin / 6;
-            if (magicMax < m.AtkMax / 6)
-                magicMax = m.AtkMax / 6;
+        var monstersText = await RenderMonsters(model);
+        var content = await InsertContentIntoTemplate(monstersText, "Ragnarok Renewal Monsters");
 
-            txtOut.Append($""" 
-
-                           <table cellpadding=4 style="width: 98%">
-                           <tr>
-                               <td style="vertical-align:top; width:20%;">
-                                   <H2 class="subheader">{name}</H2>
-                                   <br>
-                                   <center>
-                                       <img src="images/rebuildmonsters/{sprite}.png" />
-                                   </center>
-                               </td>
-                           
-                               <td style="vertical-align:top; width:22%;">
-                                   <H2 class="subheader">Stats</H2>
-                                   <table>
-                           """);
-            txtOut.AppendLine($"<tr><td><b>Level:</b></td><td>{m.Level}</td></tr>");
-            txtOut.AppendLine($"<tr><td><b>HP:</b></td><td>{m.HP:N0}</td></tr>");
-            if (!string.IsNullOrWhiteSpace(mvp))
-                txtOut.AppendLine($"<tr><td><b>Type:</b></td><td>{mvp}</td></tr><tr>");
-            txtOut.AppendLine($"<tr><td><b>Size:</b></td><td>{m.Size}</td></tr>");
-            txtOut.AppendLine($"<tr><td><b>Element:</b></td><td>{m.Element}</td></tr>");
-            txtOut.AppendLine($"<tr><td><b>Race:</b></td><td>{m.Race}</td></tr>");
-            txtOut.AppendLine($"<tr><td><b>Base Exp:</b></td><td>{m.Exp:N0}</td></tr>");
-            txtOut.AppendLine($"<tr><td><b>Job Exp:</b></td><td>{m.JobExp:N0}</td></tr>");
-            txtOut.AppendLine($"<tr><td><b>Attack Power:</b></td><td>{m.AtkMin:N0} ~ {m.AtkMax:N0}</td></tr>");
-            txtOut.AppendLine($"<tr><td><b>Magic Attack:</b></td><td>{magicMin:N0} ~ {magicMax:N0}</td></tr>");
-            txtOut.AppendLine($"<tr><td><b>Defence:</b></td><td>{m.Def}</td></tr>");
-            txtOut.AppendLine($"<tr><td><b>Magic Defence:</b></td><td>{m.MDef}</td></tr>");
-            txtOut.AppendLine($"<tr><td><b>95% Flee:</b></td><td>{m.Dex + m.Level + 75}</td></tr>");
-            txtOut.AppendLine($"<tr><td><b>100% Hit:</b></td><td>{m.Agi + m.Level + 20}</td></tr>");
-            txtOut.AppendLine($"<tr><td><b>Vit:</b></td><td>{m.Vit}</td></tr>");
-            txtOut.AppendLine($"<tr><td><b>Int:</b></td><td>{m.Int}</td></tr>");
-            txtOut.AppendLine($"<tr><td><b>Luck:</b></td><td>{m.Luk}</td></tr>");
-            txtOut.AppendLine($"""
-                               </table></td>
-                               <td style="vertical-align:top; width:25%;">
-                                   <H2 class="subheader">Drops</H2>
-                                   <table>
-                               """);
-
-            if (DataManager.MonsterDropData.TryGetValue(m.Code, out var drops))
-            {
-                foreach (var drop in drops.DropChances)
-                {
-                    var item = DataManager.ItemList[drop.Id];
-                    var countStr = "";
-                    var itemSprite = item.Name.Replace(" ", "_");
-                    if (sharedIcons.TryGetValue(itemSprite, out var copy))
-                        itemSprite = copy;
-                    if (item.ItemClass == ItemClass.Card)
-                        itemSprite = "card";
-                    if (drop.CountMax > 1)
-                        if (drop.CountMin == drop.CountMax)
-                            countStr = $"[{drop.CountMax}x]";
-                        else
-                            countStr = $"[{drop.CountMin}x-{drop.CountMax}x]";
-                    if (item.ItemClass == ItemClass.Weapon)
-                        if (DataManager.WeaponInfo.TryGetValue(item.Id, out var weapon) && weapon.CardSlots > 0)
-                            countStr = $"[{weapon.CardSlots}]";
-                    if (item.ItemClass == ItemClass.Equipment)
-                        if (DataManager.ArmorInfo.TryGetValue(item.Id, out var armor) && armor.CardSlots > 0)
-                            countStr = $"[{armor.CardSlots}]";
-                    txtOut.Append($"""
-                                    <tr>
-                                        <td style='text-align:center'><img src="images/rebuilditems/{itemSprite}.png"></td>
-                                        <td height=24>{item.Name}{countStr} ({drop.Chance / 100f}%)</td>
-                                    </tr>
-                                    
-                                   """);
-                }
-            }
-
-            txtOut.AppendLine($"</table></td>");
-            txtOut.AppendLine($"""
-                               <td style="vertical-align:top; width:33%;">
-                                <H2 class="subheader">Spawns</H2>
-                               <table>
-                               """);
-
-            string TimeSpanToTimeString(TimeSpan time)
-            {
-                var sOut = new StringBuilder();
-                if (time.Hours > 0)
-                    sOut.Append($"{time.Hours}h");
-                if (time.Minutes > 0)
-                    sOut.Append($"{time.Minutes}m");
-                if (time.Seconds > 0)
-                    sOut.Append($"{time.Seconds}s");
-                if (sOut.Length == 0)
-                    return "0s";
-                return sOut.ToString();
-            }
-
-
-            if (monsterMapSpawns.TryGetValue(m.Id, out var spawn))
-            {
-                if (spawn == null)
-                    continue;
-                var s = spawn.OrderByDescending(e => e.spawn.Count);
-                foreach (var (mapCode, mapSpawn) in spawn)
-                {
-                    var map = DataManager.Maps.FirstOrDefault(m => m.Code == mapCode);
-                    var mapName = map != null ? map.Name : mapCode;
-                    var mapSpecial = "";
-                    if (mapSpawn.DisplayType == CharacterDisplayType.Boss)
-                        mapSpecial = "<img src='images/iconboss.png' /> ";
-                    if (mapSpawn.DisplayType == CharacterDisplayType.Mvp)
-                        mapSpecial = "<img src='images/iconmvp.png' /> ";
-                    var time = "Instant";
-                    if (mapSpawn.MinSpawnTime > ServerConfig.DebugConfig.MinSpawnTime)
-                    {
-                        var min = TimeSpan.FromMilliseconds(mapSpawn.MinSpawnTime);
-                        var max = TimeSpan.FromMilliseconds(mapSpawn.MaxSpawnTime);
-                        if (min == max)
-                            time = TimeSpanToTimeString(min);
-                        else
-                            time = $"{TimeSpanToTimeString(min)} - {TimeSpanToTimeString(max)}";
-                    }
-                    txtOut.AppendLine($"<tr><td width=60%>{mapSpecial}{mapName} ({mapCode})</td><td width=15%>{mapSpawn.Count}</td><td width=25%>{time}</td>");
-                }
-            }
-
-            txtOut.AppendLine("</table></td></tr></table>");
-
-        }
-
-        var template = File.ReadAllText("template.html");
-        template = template.Replace("!!!!--MonsterArea--!!!!", txtOut.ToString());
-        File.WriteAllText(@"../../../rebuildmonsters.html", template);
+        await File.WriteAllTextAsync(@"../../../rebuildmonsters.html", content);
 
         //var world = new World();
         //NetworkManager.Init(world);
