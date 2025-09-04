72f6ea6b0e7221cbd96a891c9c1cbc592e79314d
Haoran <haoran.sun.1993@gmail.com>
2025-09-04 12:45:43 -0400

    update

--- RebuildClient/Assets/Scripts/UI/DisableWASDUINavigation.cs
+++ RebuildClient/Assets/Scripts/UI/DisableWASDUINavigation.cs
@@ -12,30 +12,10 @@ public class DisableWASDNavigation : MonoBehaviour
         Selectable[] selectables = FindObjectsOfType<Selectable>(true);
         foreach (Selectable sel in selectables)
         {
-            // Skip scrollbars if you still want arrow keys to move them
-            if (sel is Scrollbar)
-                continue;
-
             // Disable automatic navigation
             Navigation nav = sel.navigation;
             nav.mode = Navigation.Mode.None;
             sel.navigation = nav;
         }
-    }
-
-    void Update()
-    {
-        /*
-        // Block WASD and Arrow keys from selecting UI
-        if (EventSystem.current != null)
-        {
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) ||
-                Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) ||
-                Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) ||
-                Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
-            {
-                EventSystem.current.SetSelectedGameObject(null);
-            }
-        }*/
     }
 }
