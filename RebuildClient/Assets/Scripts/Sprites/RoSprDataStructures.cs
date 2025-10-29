080ed668987a26030ae9cf62ac92745fa67f2996
mamotromico <mamotromico@gmail.com>
2025-10-29 00:40:38 -0300

    Unify data structure

--- RebuildClient/Assets/Scripts/Sprites/RoSprDataStructures.cs
+++ RebuildClient/Assets/Scripts/Sprites/RoSprDataStructures.cs
@@ -2,18 +2,7 @@ using UnityEngine;
 
 namespace Assets.Scripts.Sprites
 {
-    public interface IRoSpr
-    {
-        char[] Signature { get; set; }
-        byte VersionMinor { get; set; }
-        byte VersionMajor { get; set; }
-        ushort BitmapImageCount { get; set; }
-        ushort TrueColorImageCount { get; set; }
-        TrueColorImage[] TrueColorImages { get; set; }
-        Color32[] PaletteColors { get; set; }
-    }
-
-    public class RoSprV20 : IRoSpr
+    public class RoSprData
     {
         public char[] Signature { get; set; }
         public byte VersionMinor { get; set; }
@@ -21,17 +10,6 @@ namespace Assets.Scripts.Sprites
         public ushort BitmapImageCount { get; set; }
         public ushort TrueColorImageCount { get; set; }
         public BitmapImage[] BitmapImages { get; set; }
-        public TrueColorImage[] TrueColorImages { get; set; }
-        public Color32[] PaletteColors { get; set; }
-    }
-
-    public class RoSprV21 : IRoSpr
-    {
-        public char[] Signature { get; set; }
-        public byte VersionMinor { get; set; }
-        public byte VersionMajor { get; set; }
-        public ushort BitmapImageCount { get; set; }
-        public ushort TrueColorImageCount { get; set; }
         public CompressedBitmapImage[] CompressedBitmapImages { get; set; }
         public TrueColorImage[] TrueColorImages { get; set; }
         public Color32[] PaletteColors { get; set; }
