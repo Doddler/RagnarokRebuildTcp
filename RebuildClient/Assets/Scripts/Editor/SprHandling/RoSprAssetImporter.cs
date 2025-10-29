d73f6d73d2c297a0ec89ca6c33d731cdd715d1d7
mamotromico <mamotromico@gmail.com>
2025-10-28 23:57:56 -0300

    Reformat and simplify

--- RebuildClient/Assets/Scripts/Editor/SprHandling/RoSprAssetImporter.cs
+++ RebuildClient/Assets/Scripts/Editor/SprHandling/RoSprAssetImporter.cs
@@ -18,14 +18,14 @@ namespace Assets.Scripts.Editor.SprHandling
             list.Add(SprExtension);
             EditorSettings.projectGenerationUserExtensions = list.ToArray();
         }
-        
+
         public string spriteVersion;
         public string filepath;
         public string sprFileName;
         public Texture2D palette;
         public Texture2D atlas;
-        public Rect[]  atlasRects;
-        
+        public Rect[] atlasRects;
+
         private RoSprAsset asset;
         private const string SprExtension = "spr";
 
@@ -33,18 +33,18 @@ namespace Assets.Scripts.Editor.SprHandling
         {
             asset = ScriptableObject.CreateInstance<RoSprAsset>();
             asset.Load(ctx.assetPath);
-            
+
             ctx.AddObjectToAsset(GUID.Generate().ToString(), asset.atlas);
             ctx.AddObjectToAsset(GUID.Generate().ToString(), asset.palette);
-            
+
             PopulateImporterFields();
-            
+
             ctx.AddObjectToAsset(GUID.Generate().ToString(), asset);
             ctx.SetMainObject(asset);
 
             TryIncludeSprExtension();
         }
-        
+
         private void PopulateImporterFields()
         {
             spriteVersion = asset.spriteVersion;
@@ -61,10 +61,7 @@ namespace Assets.Scripts.Editor.SprHandling
     {
         public override bool showImportedObject
         {
-            get
-            {
-                return false;
-            }
+            get { return false; }
         }
     }
 }
--- RebuildClient/Assets/Scripts/Sprites/RoSprDataStructures.cs
+++ RebuildClient/Assets/Scripts/Sprites/RoSprDataStructures.cs
@@ -1,4 +1,3 @@
-using System.Runtime.InteropServices;
 using UnityEngine;
 
 namespace Assets.Scripts.Sprites
@@ -6,21 +5,21 @@ namespace Assets.Scripts.Sprites
     public interface IRoSpr
     {
         char[] Signature { get; set; }
-        byte VersionMajor { get; set; }
         byte VersionMinor { get; set; }
+        byte VersionMajor { get; set; }
         ushort BitmapImageCount { get; set; }
         ushort TrueColorImageCount { get; set; }
         TrueColorImage[] TrueColorImages { get; set; }
         Color32[] PaletteColors { get; set; }
     }
-    
+
     public struct BitmapImage
     {
         public ushort ImageWidth;
         public ushort ImageHeight;
         public byte[] PaletteIndexes;
     }
-    
+
     public struct CompressedBitmapImage
     {
         public ushort ImageWidth;
@@ -28,31 +27,31 @@ namespace Assets.Scripts.Sprites
         public ushort CompressedSize;
         public byte[] CompressedPaletteIndexes;
     }
-    
+
     public struct TrueColorImage
     {
         public ushort ImageWidth;
         public ushort ImageHeight;
         public Color32[] ImageData;
     }
-    
-    public struct RoSprV20 : IRoSpr
+
+    public class RoSprV20 : IRoSpr
     {
         public char[] Signature { get; set; }
-        public byte VersionMajor { get; set; }
         public byte VersionMinor { get; set; }
+        public byte VersionMajor { get; set; }
         public ushort BitmapImageCount { get; set; }
         public ushort TrueColorImageCount { get; set; }
         public BitmapImage[] BitmapImages { get; set; }
         public TrueColorImage[] TrueColorImages { get; set; }
         public Color32[] PaletteColors { get; set; }
     }
-    
-    public struct RoSprV21 : IRoSpr
+
+    public class RoSprV21 : IRoSpr
     {
         public char[] Signature { get; set; }
-        public byte VersionMajor { get; set; }
         public byte VersionMinor { get; set; }
+        public byte VersionMajor { get; set; }
         public ushort BitmapImageCount { get; set; }
         public ushort TrueColorImageCount { get; set; }
         public CompressedBitmapImage[] CompressedBitmapImages { get; set; }
